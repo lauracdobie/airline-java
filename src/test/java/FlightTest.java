ed45417bfe7393f1e3a4bce326d1c4901ee43dc4
Laura Dobie <moving.toyshop@gmail.com>
2021-01-29 15:19:46 +0000

    Can create a flight with all the correct details.

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class FlightTest {
 
@@ -19,17 +20,22 @@ public class FlightTest {
         hopefulPuffin = new Plane(PlaneType.HOPEFULPUFFIN404, 200, 30000);
         fullPlane = new Plane(PlaneType.GLIDINGGUILLEMOT403, 0, 15000);
         flight1 = new Flight(hopefulPuffin, "EZY3498", IATACode.EDI, IATACode.GLA, "14:00");
-        flight2 = new Flight(fullPlane, "EZY3498", IATACode.EDI, IATACode.GLA, "14:00");
+        flight2 = new Flight(fullPlane, "EZY6142", IATACode.LGW, IATACode.NCL, "07:00");
+    }
+
+    @Test
+    public void canCreateFlight() {
+        assertNotNull(flight1);
+        assertEquals(hopefulPuffin, flight1.getPlane());
+        assertEquals(200, flight1.getAvailableSeats());
+        assertEquals(IATACode.GLA, flight1.getDepartureAirport());
+        assertEquals(IATACode.EDI, flight1.getDestination());
+        assertEquals("14:00", flight1.getDepartureTime());
     }
 
     @Test
     public void checkPassengerListStartsEmpty() {
         assertEquals(0, flight1.getNumberOfPassengers());
-    }
-
-    @Test
-    public void checkFlightHasPlane() {
-        assertEquals(hopefulPuffin, flight1.getPlane());
     }
 
     @Test
