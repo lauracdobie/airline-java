6d9f624d46b5683c5524659a26d257b77751df62
Laura Dobie <moving.toyshop@gmail.com>
2021-01-29 15:05:50 +0000

    Created flight class with methods. Checked passenger list starts empty.

---
+++ src/main/java/Flight.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Flight {
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private IATACode destination;
+    private IATACode departureAirport;
+    private String departureTime;
+
+    public Flight (Plane plane, String flightNumber, IATACode destination, IATACode departureAirport, String departureTime) {
+        this.plane = plane;
+        this.passengers = new ArrayList<Passenger>();
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    public int getNumberOfPassengers() {
+        return this.passengers.size();
+    }
+
+    public int getAvailableSeats() {
+        return this.plane.getCapacity();
+    }
+
+    public String getFlightNumber() {
+        return this.flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public IATACode getDestination() {
+        return this.destination;
+    }
+
+    public void setDestination(IATACode destination) {
+        this.destination = destination;
+    }
+
+    public IATACode getDepartureAirport() {
+        return this.departureAirport;
+    }
+
+    public void setDepartureAirport(IATACode departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return this.departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void addPassengerToFlight(Passenger passenger) {
+        if (this.plane.getCapacity() > 0){
+            this.passengers.add(passenger);
+            this.plane.setCapacity(this.plane.getCapacity() - 1);
+        }
+    }
+}
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+
+    Passenger jetSetter1;
+    Passenger jetSetter2;
+    Plane hopefulPuffin;
+    Plane fullPlane;
+    Flight flight1;
+    Flight flight2;
+
+    @Before
+    public void before() {
+        jetSetter1 = new Passenger("Giovanni Rosso", 2);
+        jetSetter2 = new Passenger("Mercedes Lindo", 1);
+        hopefulPuffin = new Plane(PlaneType.HOPEFULPUFFIN404, 200, 30000);
+        fullPlane = new Plane(PlaneType.GLIDINGGUILLEMOT403, 0, 15000);
+        flight1 = new Flight(hopefulPuffin, "EZY3498", IATACode.EDI, IATACode.GLA, "14:00");
+        flight2 = new Flight(fullPlane, "EZY3498", IATACode.EDI, IATACode.GLA, "14:00");
+    }
+
+    @Test
+    public void checkPassengerListStartsEmpty() {
+        assertEquals(0, flight1.getNumberOfPassengers());
+    }
+}
