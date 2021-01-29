101e1410b5e47a52f9308c95a482c3fadb839faa
Laura Dobie <moving.toyshop@gmail.com>
2021-01-29 11:59:42 +0000

    Passenger class set up, tests passing.

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,25 @@
+public class Passenger {
+    private String name;
+    private int bags;
+
+    public Passenger(String name, int bags){
+        this.name = name;
+        this.bags = bags;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getNumberOfBags() {
+        return this.bags;
+    }
+
+    public void setNumberOfBags(int bags) {
+        this.bags = bags;
+    }
+}
---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger jetSetter;
+
+    @Before
+    public void before() {
+        jetSetter = new Passenger("Giovanni Rosso", 2);
+    }
+
+    @Test
+    public void checkPassengerHasName() {
+        assertEquals("Giovanni Rosso", jetSetter.getName());
+    }
+
+    @Test
+    public void checkPassengerNameCanBeSet() {
+        jetSetter.setName("John Red");
+        assertEquals("John Red", jetSetter.getName());
+    }
+
+    @Test
+    public void checkPassengerHasBags() {
+        assertEquals(2, jetSetter.getNumberOfBags());
+    }
+
+    @Test
+    public void checkPassengerBagsCanBeSet() {
+        jetSetter.setNumberOfBags(3);
+        assertEquals(3, jetSetter.getNumberOfBags());
+    }
+}
